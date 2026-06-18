f2d590a42adc36b79f9df83df630c8f4e8d33d92
md abu jobail <mabujobail@gmail.com>
2026-06-18 19:28:15 +0600

    feat: implement abstract Person model with encapsulation

---
+++ src/models/Person.java
@@ -0,0 +1,43 @@
+package models;
+
+public abstract class Person {
+
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(String name, int age, int id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void displayInfo(){
+System.out.println("my name is "+ getName() + " "+ " my age "+ getAge()+" "+ " my id "+ getId());
+    }
+
+}
