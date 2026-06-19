1c6ab2044fcf3a9dc0396a6678b170010c1f628b
md abu jobail <mabujobail@gmail.com>
2026-06-19 09:49:05 +0600

    feat: implement Student model with inheritance and method overriding

---
+++ src/models/Student.java
@@ -0,0 +1,27 @@
+package models;
+
+
+public class Student extends Person {
+    private double gpa;
+
+    public Student(String name, int age, int id, double gpa) {
+        super(name, age, id);
+        this.gpa = gpa;
+    }
+
+    public double getGpa() {
+        return gpa;
+
+    }
+
+    public void setGpa(double x) {
+        this.gpa = x;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println(" my gpa is " + getGpa());
+    }
+
+}
